6631328b81de9ee8c77f2cb0a854226e83cc8d0d
LeeJuOh <xop4p@naver.com>
2024-11-17 00:34:33 +0900

    init

---
+++ src/main/kotlin/jolee/study/til/TilApplication.kt
@@ -0,0 +1,11 @@
+package jolee.study.til
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TilApplication
+
+fun main(args: Array<String>) {
+	runApplication<TilApplication>(*args)
+}
---
+++ src/test/kotlin/jolee/study/til/TilApplicationTests.kt
@@ -0,0 +1,13 @@
+package jolee.study.til
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TilApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
